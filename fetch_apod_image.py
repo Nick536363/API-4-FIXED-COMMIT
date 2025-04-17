4332dd476d58a3ca8ee3ae8b5ea47c925358436c
Nick536363 <134803463+Nick536363@users.noreply.github.com>
2025-04-17 16:28:24 +0200

    Добавил скрипт для скачивания картинок (APOD)

---
+++ fetch_apod_image.py
@@ -0,0 +1,26 @@
+from requests import get
+from save_tools import download_image
+from argparse import ArgumentParser
+from os import getenv
+
+
+def main(api_key):
+    parser = ArgumentParser()
+    parser.add_argument("--folder", type=str, default="./", help="Папка, куда будут скачаны картинки")
+    parser.add_argument("count", type=int, help="Кол-во скачиваемых картинок")
+    args = parser.parse_args()
+    url_apod = "https://api_key.nasa.gov/planetary/apod"
+    payload = {
+        "api_key": api_key,
+        "count": args.count
+    }
+    response_apod = get(url_apod, params=payload)
+    response_apod.raise_for_status()
+    for element_num, content in enumerate(response_apod.json(), 1):
+        filename = f"apod_{element_num}.png"
+        apod_link = content["url"]
+        download_image(apod_link, args.folder, filename, api_key=api_key)
+
+
+if __name__ == "__main__":
+    main(getenv("APOD_EPIC_API"))
